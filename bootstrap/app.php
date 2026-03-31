7a32b79f762dabc5a056415801d72c5860d61c78
Andrade . <j0s3.90jr@gmail.com>
2026-03-31 16:06:27 +0200

    Fix CSRF for API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,21 +7,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        api: __DIR__.'/../routes/api.php', // Asegúrate de que esta línea existe
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+    ->withMiddleware(function (Middleware $middleware) {
+        // EXCLUIMOS LAS RUTAS DE LA API DEL CHEQUEO CSRF
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
         ]);
-
-        $middleware->alias([
-            'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-        ]);
-
-        //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
