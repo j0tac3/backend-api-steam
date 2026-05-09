7c1456aa19dd4f1f4423180ce65827088b7e30e1
Andrade . <j0s3.90jr@gmail.com>
2026-05-09 23:24:08 +0200

    todo 2

--- resources/views/social_preview.blade.php
+++ resources/views/social_preview.blade.php
@@ -2,15 +2,17 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Biblioteca de {{ $user->username }}</title>
     
+    <title>🎮 Colección de juegos de {{ $user->username }}</title>
+    <meta name="description" content="🏆 Completados: {{ $completados }} | 🎮 Jugando: {{ $jugando }} | ⏳ Pendientes: {{ $pendientes }}">
+    
+    <meta name="theme-color" content="#66c0f4">
+
+    <meta property="og:type" content="website">
     <meta property="og:title" content="🎮 Colección de juegos de {{ $user->username }}">
     <meta property="og:description" content="🏆 Completados: {{ $completados }} | 🎮 Jugando: {{ $jugando }} | ⏳ Pendientes: {{ $pendientes }}">
     <meta property="og:image" content="{{ $imageUrl }}">
-    <meta property="og:image:width" content="1200">
-    <meta property="og:image:height" content="630">
     <meta property="og:url" content="{{ $frontendUrl }}">
-    <meta property="og:type" content="profile">
 
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="🎮 Colección de juegos de {{ $user->username }}">
@@ -18,7 +20,6 @@
     <meta name="twitter:image" content="{{ $imageUrl }}">
 </head>
 <body>
-    <p>Redirigiendo a la biblioteca...</p>
-    <script>window.location.href = "{{ $frontendUrl }}";</script>
+    <script>window.location.replace("{{ $frontendUrl }}");</script>
 </body>
 </html>
